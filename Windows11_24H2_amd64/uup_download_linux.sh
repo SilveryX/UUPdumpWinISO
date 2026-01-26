a19416a7b47ed1a1a6de53b8eb16371f9c9faa7e
yuanpeirong <yuanpeirong@vip.qq.com>
2026-01-26 16:24:24 +0800

    24H2(26100.7628)、25H2(26200.7628)

--- Windows11_24H2_amd64/uup_download_linux.sh
+++ Windows11_24H2_amd64/uup_download_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Generated on 2026-01-26 06:11:12 GMT
+#Generated on 2026-01-26 08:21:42 GMT
 
 # Proxy configuration
 # If you need to configure a proxy to be able to connect to the internet,
@@ -47,7 +47,7 @@ fi
 
 echo ""
 echo "Retrieving aria2 script for the UUP set..."
-aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=13f2d59a-faca-40c7-90ae-8537904c9708&pack=zh-cn&edition=professional&aria2=2"
+aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=d727e3b6-343f-422c-bfd4-43b64bbfd1f9&pack=zh-cn&edition=professional&aria2=2"
 if [ $? != 0 ]; then
   echo "Failed to retrieve aria2 script"
   exit 1
