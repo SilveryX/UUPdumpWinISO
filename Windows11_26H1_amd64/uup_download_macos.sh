dea146476aebdd7ac40d5d32938841e1a196ad88
SilveryX <67977075+SilveryX@users.noreply.github.com>
2026-03-02 10:26:13 +0800

    Add files via upload

--- Windows11_26H1_amd64/uup_download_macos.sh
+++ Windows11_26H1_amd64/uup_download_macos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Generated on 2026-02-12 12:30:28 GMT
+#Generated on 2026-03-02 02:22:15 GMT
 
 # Proxy configuration
 # If you need to configure a proxy to be able to connect to the internet,
@@ -47,7 +47,7 @@ fi
 
 echo ""
 echo "Retrieving aria2 script for the UUP set..."
-aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=5c22b6ed-4787-49c9-85eb-03a709470097&pack=zh-cn&edition=professional&aria2=2"
+aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=3a945332-7028-432a-a32b-26f3edfc4759&pack=zh-cn&edition=professional&aria2=2"
 if [ $? != 0 ]; then
   echo "Failed to retrieve aria2 script"
   exit 1
